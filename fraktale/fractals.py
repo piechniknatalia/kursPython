15ead4b0cf524253647678ac072279260171b5a9
Natalia Piechnik <nat.piechnik@gmail.com>
2022-01-03 14:09:09 +0100

    projekt2

--- fraktale/fractals.py
+++ fraktale/fractals.py
@@ -1,45 +1,56 @@
 # Modul z fraktalami
 
 import math
+import turtle
 
-def tree(color1, color2, t, length):
+def set_turtle():
+    turtle.bgcolor("black")
+    turtle1 = turtle.Turtle()
+    turtle1.hideturtle()
+    return turtle1
+
+def tree(color1, color2, turtle, length):
+    """Funkcja rysujaca fraktal przypominajacy drzewo za pomoca modulu turtle"""
+    turtle.color(color1)
     angle = 30
     length_change = 5
     miniumum_length = 5
     if length > miniumum_length:
-        t.color(color1)
-        t.forward(length)
+        turtle.color(color1)
+        turtle.forward(length)
         new_length = length - length_change
 
-        t.left(angle)
-        tree(color1, t, new_length)
+        turtle.left(angle)
+        tree(color1, color2, t, new_length)
 
-        t.right(2 * angle)
-        tree(color2, t, new_length)
+        turtle.right(2 * angle)
+        tree(color2, color1, t, new_length)
 
-        t.left(angle)
-        t.color(color1)
-        t.backward(length)
+        turtle.left(angle)
+        turtle.color(color1)
+        turtle.backward(length)
+
 
 def snowflake(color1, color2, turtle, iterations, length):
+    """Funkcja rysujaca fraktal przypominajacy sniezynke za pomoca modulu turtle"""
     turtle.color(color1)
     if iterations == 0:
         turtle.forward(length)
     else:
         iterations = iterations - 1
         length = length / 3
-        snowflake(color1, turtle, iterations, length)
+        snowflake(color1, color2, turtle, iterations, length)
         turtle.left(60)
-        snowflake(color2, turtle, iterations, length)
+        snowflake(color2, color1, turtle, iterations, length)
         turtle.right(60 * 2)
-        snowflake(color1, turtle, iterations, length)
+        snowflake(color1, color2, turtle, iterations, length)
         turtle.left(60)
-        snowflake(color2, turtle, iterations, length)
-
-
+        snowflake(color2, color1, turtle, iterations, length)
 
 
 def fiboshell(color1, color2, turtle, n):
+    """Funkcja rysujaca fraktal przypominajacy muszle za pomoca modulu turtle"""
+    turtle.color(color1)
     factor = 3
     a = 0
     b = 1
@@ -56,3 +67,32 @@ def fiboshell(color1, color2, turtle, n):
         temp = a
         a = b
         b = temp + b
+
+
+def draw(turtle, type, size, color1, color2):
+    """Funkcja rysujaca fraktale w zaleznosci od podanych danych"""
+    # zbior kolorow do wyboru
+    set_of_colors = {'1': 'brown1',
+                     '2': 'chocolate1',
+                     '3': 'DarkGoldenrod2',
+                     '4': 'DarkOliveGreen3',
+                     '5': 'CadetBlue',
+                     '6': 'blue2',
+                     '7': 'BlueViolet'}
+    if type == 'd':
+        length = 20 + (size-1) * 3
+        turtle.setheading(90)
+        tree(set_of_colors[str(color1)], set_of_colors[str(color2)], turtle, length)
+    elif type == 's':
+        length = round(1 + (size - 1) * 0.3)
+        turtle.up()
+        turtle.forward(300/2)
+        turtle.left(180)
+        turtle.down()
+        turtle.right(90)
+        for i in range(3):
+            snowflake(set_of_colors[str(color1)], set_of_colors[str(color2)], turtle, length, 300)
+            turtle.right(120)
+    elif type == 'm':
+        length = round(1 + (size - 1) * 1.2)
+        fiboshell(set_of_colors[str(color1)], set_of_colors[str(color2)], turtle, length)
--- fraktale/main.py
+++ fraktale/main.py
@@ -1,10 +1,9 @@
-import turtle
 import fractals
-LENGTH = 30 #zmieniane przez uzytkownika (20 - 50 tree) (1-12 muszla)
-ITERATIONS = 3 # tylko do sniezynki
 
+#zbior fraktali do wyboru
 set_of_types = {'d', 's', 'm'}
-type = input("Jaki fraktal chcesz narysowac? Do wyboru mamy drzewo, sniezynke i muszle. Wybierz d, s lub m :)\n")
+
+type = input("Jaki fraktal chcesz narysowac? Do wyboru mamy drzewo, sniezynke i muszle.\nWybierz d, s lub m :)\n")
 while type not in set_of_types:
     type = input("Do wyboru masz jedynie d dla drzewa, s dla sniezynki i m dla muszli.\n")
 
@@ -19,14 +18,6 @@ while True:
             print("Liczba nie jest z odpowiedniego przedzialu")
         else:
             break
-
-set_of_colors = {'czerwony': 'brown1',
-                 'pomaranczowy': 'chocolate1',
-                 'zolty': 'DarkGoldenrod2',
-                 'zielony':'DarkOliveGreen3',
-                 'niebieski' :'CadetBlue',
-                 'granatowy' : 'blue2',
-                 'fioletowy':'BlueViolet'}
 
 while True:
     x = input("Bedziemy rysowac dwukolorowy fraktal, jaki bedzie pierwszy kolor"
@@ -55,63 +46,11 @@ while True:
         else:
             break
 
-turtle.bgcolor("black")
-turtle.pensize(800) # moze zmienic jeszcze
-turtle1 = turtle.Turtle()
-turtle1.hideturtle()
-turtle1.color(color1)
 
-if type == 'd':
-    length = 20 + (size-1) * 3
-    turtle1.setheading(90)
-    fractals.tree(color1, color2, turtle1, length)
-
-elif type == 's':
-    length = round(1 + (size - 1) * 0.5)
-    turtle1.up()
-    turtle1.forward(LENGTH/2)
-    turtle1.left(180)
-    turtle1.down()
-    turtle.right(90)
-    for i in range(3):
-        fractals.snowflake(color1, color2, turtle1, length, 300)
-        turtle1.right(120)
-
-elif type == 'm':
-    length = round(1 + (size - 1) * 1.2)
-    fractals.fiboshell(color1, color2, turtle1, length)
-
-turtle1.mainloop()
+fractals.draw(fractals.set_turtle, type, size, color1, color2)
 
 
 
 
-
-#LENGTH = 300 dla śnieżynki
-
-
-
-
-
-
-
-
-
-
-
-
-#fiboshell(turtle1, 12)
-
-#SNOW
-#turtle1.up()
-#turtle1.forward(LENGTH/2)
-#turtle1.left(180)
-#turtle1.down()
-#turtle.right(90)
-#for i in range(3):
-#    snowflake(COLOR2, turtle1, ITERATIONS, LENGTH)
-#    turtle1.right(120)
-
-#
 
 
